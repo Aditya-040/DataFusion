5ffc35a1b57c88321529766fb6f5197d9fca82f0
King Lee <c-king.lee@hulu.com>
2024-06-21 08:02:10 -0600

    modal interaction fixed

---
+++ src/app/home/customers/CustomerTable.tsx
@@ -0,0 +1,16 @@
+import Table from "@/components/Table";
+import React from "react";
+
+export default function CustomerTable({data}: any) {
+
+   return <Table
+       columns={[
+           {title: '', key: 'checkbox', width: 'w-[50p]'},
+           {title: 'Name', key: 'name', width: 'w-[50p]'},
+           {title: 'Price', key: 'price', width: 'w-[25p]'},
+           {title: 'Description', key: 'description', width: 'w-[25p]'},
+           {title: '', key: 'tools', width: 'w-[25p]'},
+       ]}
+       data={data || []}
+   />
+}
